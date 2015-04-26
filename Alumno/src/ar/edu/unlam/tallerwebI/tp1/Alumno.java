61b5d3a487586dd229030653f855a72f45a0445d
mauroef <cypress90@gmail.com>
2015-04-26 15:35:41 -0300

    TP 1

---
+++ Alumno/src/ar/edu/unlam/tallerwebI/tp1/Alumno.java
@@ -0,0 +1,47 @@
+package ar.edu.unlam.tallerwebI.tp1;
+
+public class Alumno {
+	int primerParcial;
+	int segundoParcial;
+	
+	public int getPrimerParcial() {
+		return primerParcial;
+	}
+	
+	public int getSegundoParcial() {
+		return segundoParcial;
+	}
+	
+	public void calificarPrimerParcial(int nota) {
+		primerParcial = nota;
+	}
+	
+	public void calificarSegundoParcial(int nota) {
+		segundoParcial = nota;
+	}
+	
+	public boolean estaAprobado(){
+		if(primerParcial >= 4){
+			if(segundoParcial >=4){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean estaPromocionado(){
+		if(primerParcial >= 7){
+			if(segundoParcial >=7){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+
+
+
+	
+	
+}
